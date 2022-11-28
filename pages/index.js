9a380db935cf576c41c85aa3d19f17918c8224ee
dev-rome <haynesjerome@gmail.com>
2022-11-28 12:39:12 -0500

    edit index to show logo and button to enter site

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,26 @@
-import Head from 'next/head'
-import Image from 'next/image'
-
+import Head from "next/head";
+import Link from "next/link";
+import Image from "next/image";
+import Logo from "../public/images/logo.png";
 
 const Welcome = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+    <section>
+      <div className="max-w-screen-lg mx-auto">
+        <div className="grid place-content-center h-screen">
+          <div className="flex flex-col items-center gap-y-3">
+            <Image src={Logo} alt="logo" width="400" priority />
+            <Link
+              className="bg-pink text-white tracking-wide py-1 px-5 hover:duration-300 hover:ease-linear hover:bg-white hover:text-pink md:text-lg"
+              href="/home"
+            >
+              Enter
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Welcome
+export default Welcome;
