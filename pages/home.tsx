3893bb4b4c875509cc9f7bc880b4338f4e468e8f
dev-rome <haynesjerome@gmail.com>
2022-12-02 15:24:13 -0500

    added motion to home to fade in page content and imported animate.css

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,23 +1,41 @@
-import Link from "next/link";
 import Navbar from "../components/navbar/Navbar";
 import Hero from "../components/hero/Hero";
 import About from "../components/about/About";
+import Portfolio from "../components/portfolio/Portfolio";
+import Contact from "../components/contact/Contact";
+import Footer from "../components/footer/footer";
+import { motion } from "framer-motion";
+import "animate.css";
 
 const Home = () => {
   return (
-    <>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{
+        duration: 1,
+        ease: "easeInOut",
+      }}
+    >
       <header>
         <Navbar />
       </header>
-
-      <section>
+      <section id="home">
         <Hero />
       </section>
-
-      <section>
-      <About />
+      <section id="about">
+        <About />
       </section>
-    </>
+      <section id="portfolio">
+        <Portfolio />
+      </section>
+      <section id="contact">
+        <Contact />
+      </section>
+      <footer>
+        <Footer />
+      </footer>
+    </motion.div>
   );
 };
 
