61758d5fb87bee856b825d7a00097c661ced30fc
dev-rome <haynesjerome@gmail.com>
2022-12-02 15:30:49 -0500

    file to show navbar with framer motion animations

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,57 +1,39 @@
 import NavbarItem from "./NavbarItem";
-import NavToggle from "./NavToggle";
-import { FaHome, FaUser, FaBriefcase, FaEnvelope } from "react-icons/fa";
-import { motion, useCycle, Variants } from "framer-motion";
-
-const sideNavbar: Variants = {
-  open: {
-    clipPath: `circle(1000px at 40px 40px)`,
-    transition: {
-      duration: 0.4,
-    },
-  },
-  closed: {
-    clipPath: `circle(30px at 40px 40px)`,
-    transition: {
-      duration: 0.4,
-      delay: 0.4,
-    },
-  },
-};
-
-const sideNavItems: Variants = {
-  open: {
-    transition: { staggerChildren: 0.07, delayChildren: 0.2 },
-  },
-  closed: {
-    transition: { staggerChildren: 0.05, staggerDirection: -1 },
-  },
-};
+import { FaLinkedin, FaGithub } from "react-icons/fa";
+import { motion } from "framer-motion";
 
 const Navbar = () => {
-  const [isOpen, toggleOpen] = useCycle(false, true);
-
   return (
-    <motion.nav
-      className="fixed top-0 bottom-0 w-[100px] z-10"
-      initial={false}
-      animate={isOpen ? "open" : "closed"}
-    >
-      <motion.div
-        className="bg-white fixed top-0 left-0 bottom-0 w-[100px]"
-        variants={sideNavbar}
-      />
-      <NavToggle toggle={() => toggleOpen()} />
-      <motion.ul
-        className="flex flex-col justify-center gap-7 p-6 absolute top-[250px] w-[160]"
-        variants={sideNavItems}
-      >
-        <NavbarItem url={"/"} icon={<FaHome />} />
-        <NavbarItem url={"/"} icon={<FaUser />} />
-        <NavbarItem url={"/"} icon={<FaBriefcase />} />
-        <NavbarItem url={"/"} icon={<FaEnvelope />} />
-      </motion.ul>
-    </motion.nav>
+    <nav className="fixed w-full z-10 bg-black flex justify-between items-center p-5">
+      <ul className="flex gap-4">
+        <NavbarItem url={"#home"} title={"Home"} />
+        <NavbarItem url={"#about"} title={"About"} />
+        <NavbarItem url={"#portfolio"} title={"Portfolio"} />
+        <NavbarItem url={"#contact"} title={"Contact"} />
+      </ul>
+      <div className="flex items-center gap-3 text-white">
+        <motion.a
+          className=" hover:text-pink"
+          href="https://www.linkedin.com/in/jerome-haynes/"
+          target="_blank"
+          rel="noopener noreferrer"
+          whileHover={{ scale: 1.1 }}
+          transition={{ type: "spring", stiffness: 400, damping: 10 }}
+        >
+          <FaLinkedin />
+        </motion.a>
+        <motion.a
+          className=" hover:text-pink"
+          href="https://www.linkedin.com/in/jerome-haynes/"
+          target="_blank"
+          rel="noopener noreferrer"
+          whileHover={{ scale: 1.1 }}
+          transition={{ type: "spring", stiffness: 400, damping: 10 }}
+        >
+          <FaGithub />
+        </motion.a>
+      </div>
+    </nav>
   );
 };
 
