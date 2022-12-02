5da7fae2cfd304662a20f2baccbe0206700ed345
dev-rome <haynesjerome@gmail.com>
2022-12-02 15:28:57 -0500

    file to pass navbar item to navbar

--- components/navbar/NavbarItem.tsx
+++ components/navbar/NavbarItem.tsx
@@ -1,36 +1,14 @@
 import Link from "next/link";
-import { motion, Variants } from "framer-motion";
+export interface NavbarItemProps {
+  url: string;
+  title: string;
+}
 
-const NavbarItemVariants: Variants = {
-  open: {
-    y: 0,
-    opacity: 1,
-    transition: {
-      duration: 0.4,
-
-      ease: [0.6, 0.05, -0.01, 0.9],
-    },
-  },
-  closed: {
-    y: 50,
-    opacity: 0,
-    transition: {
-      duration: 0.4,
-      ease: [0.6, 0.05, -0.01, 0.9],
-    },
-  },
-};
-
-const NavbarItem = ({ url, icon }: { url: string; icon: JSX.Element }) => {
+const NavbarItem = ({ url, title }: NavbarItemProps) => {
   return (
-    <div className="flex items-center gap-2">
-      <motion.li
-        className="flex items-center justify-center bg-pink text-white h-7 w-7 rounded-full"
-        variants={NavbarItemVariants}
-      >
-        <Link href={url}>{icon}</Link>
-      </motion.li>
-    </div>
+    <li className="text-white hover:text-pink">
+      <Link href={url}>{title}</Link>
+    </li>
   );
 };
 
